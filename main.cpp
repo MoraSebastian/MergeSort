89ee111dad8a3771266eac2323c960dca036b4e5
anferente20 <34009575+anferente20@users.noreply.github.com>
2018-03-11 19:38:42 -0500

    Add files via upload
    MergeSort final y gráficas

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include<stdlib.h>
-#include<time.h>
+#include <ctime> 
+#include <time.h>
+#include <stdio.h>
+#include <windows.h>
+
 using namespace std;
 void mergeList(int *lista, int inicio1 , int fin1,int inicio2, int fin2);
 void mergeSort(int *lista, int prim , int fin){
@@ -11,7 +14,12 @@ void mergeSort(int *lista, int prim , int fin){
 		mergeList(lista,prim,medio,medio+1,fin);
 	}
 }
-
+double performancecounter_diff(LARGE_INTEGER *a, LARGE_INTEGER *b)
+{
+  LARGE_INTEGER freq;
+  QueryPerformanceFrequency(&freq);
+  return (double)(a->QuadPart - b->QuadPart) / (double)freq.QuadPart;
+}
 void mergeList(int *lista, int inicio1 , int fin1,int inicio2, int fin2){
 	int *aux = new int[fin2-inicio1+1];
 	int i;	
@@ -45,25 +53,39 @@ void mergeList(int *lista, int inicio1 , int fin1,int inicio2, int fin2){
 	}
 	ayuda = 0;
 	for(i=Inicio;i<=Final;i++){
-		cout<<aux[ayuda]<<" ";
+		//cout<<aux[ayuda]<<" ";
 		lista[i] = aux[ayuda];
 		ayuda +=1;
 	}
-	cout<<"\n"<<endl;
+	//cout<<"\n"<<endl;
 	
 }
 
 int main(int argc, char** argv) {
 	srand(time(NULL));
-	int n = 16;
+	LARGE_INTEGER t_ini, t_fin;// Variables que se usaran para medir el tiempo del programa 
+   	double secs;//Segundos que se tarda el programa en ser ejecutado
+	int n = 50;
+	
+	while(n<=500){
 	int *arreglo = new int [n];
 	
 	for(int i = 0;i<n;i++){
 		arreglo[i] = rand()%100;
-		cout<<arreglo[i]<<" ";
+		
 	}
-	cout<<endl;
+
+
+	QueryPerformanceCounter(&t_ini);
 	mergeSort(arreglo,0,15);
+	QueryPerformanceCounter(&t_fin);
+   	secs = performancecounter_diff(&t_fin, &t_ini);
+	cout<<" "<<secs * 1000.0 <<endl;
+   	
+   	delete[] arreglo;
+	n+=10;
+	}
+	
 	
 	
 	return 0;
